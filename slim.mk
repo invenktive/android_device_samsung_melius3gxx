58298efad7cb137a95afd9b02cf6b5573367f5d0
Silesh K Nair <sileshn@gmail.com>
2015-05-16 08:15:11 +0530

    Changes for slim

--- slim.mk
+++ slim.mk
@@ -14,26 +14,22 @@
 # limitations under the License.
 #
 
+$(call inherit-product, device/samsung/melius3gxx/full_melius3gxx.mk)
+
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/melius3gxx/full_melius3gxx.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # product
 PRODUCT_DEVICE := melius3gxx
 PRODUCT_BRAND := Samsung
-PRODUCT_NAME := cm_melius3gxx
+PRODUCT_NAME := slim_melius3gxx
 PRODUCT_MODEL := GT-I9200
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 # override
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_NUMBER=I9200XXUDNE4 \
-    TARGET_DEVICE=meliuslte \
-    TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
     PRIVATE_BUILD_DESC="melius3gxx-user 4.4.2 KOT49H I9200XXUDNE4 release-keys" \
     BUILD_FINGERPRINT="samsung/melius3gxx/melius3g:4.4.2/KOT49H/I9200XXUDNE4:user/release-keys"
